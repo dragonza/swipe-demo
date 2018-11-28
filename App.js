c5a682bc8c284708f90c269b8e450b5f6ead18a7
ngoc.vuong <ngoc.vuong@nfq.asia>
2018-11-28 23:33:33 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+  Image
+} from 'react-native';
+import { Card, Button, Icon } from 'react-native-elements';
+import Swipe from './Swipe';
+import jobs from './data';
+
+class App extends Component {
+  state = {
+    likedJobs: 0,
+    passedJobs: 0
+  };
+
+  handleLikedJob = () => {
+    this.setState(({ likedJobs }) => ({
+      likedJobs: likedJobs + 1
+    }));
+  };
+
+  handlePassedJob = () => {
+    this.setState(({ passedJobs }) => ({
+      passedJobs: passedJobs + 1
+    }));
+  };
+
+  renderCards(job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.02
+    };
+
+    return (
+      <Card title={job.jobtitle} titleStyle={{ fontSize: 14 }}>
+        <View style={{ height: 200 }}>
+          <Image
+            source={require('./assets/image.jpg')}
+            style={{ width: '100%', height: 200 }}
+          />
+        </View>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text numberOfLines={4}>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}
+        </Text>
+      </Card>
+    );
+  }
+
+  renderNoMoreCards = () => {
+    return (
+      <Card title="No More cards">
+        <Button
+          title="Back To Map"
+          large
+          icon={{ name: 'my-location' }}
+          backgroundColor="#03A9F4"
+        />
+      </Card>
+    );
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.statusStyle}>
+          <Text style={{ color: 'red' }}>Passed: {this.state.passedJobs}</Text>
+          <Text style={{ color: 'blue' }}>Like: {this.state.likedJobs}</Text>
+        </View>
+        <Swipe
+          onSwipeRight={this.handleLikedJob}
+          onSwipeLeft={this.handlePassedJob}
+          keyProp="jobkey"
+          data={jobs}
+          renderCard={this.renderCards}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  statusStyle: {
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  detailWrapper: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10
+  }
+});
+
+export default App;
---
+++ data.js
@@ -0,0 +1,73 @@
+export default [
+  {
+    "jobtitle": "Java Developer",
+    "company": "ABC Corporation",
+    "snippet": "a <b>Java</b> Developer to join our team. This position will be responsible for design and development of <b>Java</b>... <b>Java</b> or C# Frameworks/Skills: <b>Java</b> EE, <b>Java</b> Swing or... ",
+    "jobkey": "83400e947276d20b",
+    "sponsored": false,
+    "formattedRelativeTime": "1 day ago",
+  },
+  {
+    "jobtitle": "Sr. Java Developer",
+    "company": "CoreLogic",
+    "snippet": "Knowledge/Skills Strong <b>Java</b> development skills... abreast of emerging technology trends Core <b>Java</b> and <b>Java</b> Enterprise Edition (JavaEE) Strong relational... ",
+    "jobkey": "9d2c6535dc61d3b6",
+    "formattedRelativeTime": "4 days ago",
+  },
+  {
+    "jobtitle": "Software Developer III - US Job",
+    "company": "Rackspace",
+    "snippet": "JavaScript and some of the following languages: Scala Lisp Erlang Haskell Clojure <b>Java</b> Ruby C# Smalltalk Python , Can energetically discuss technical opinions... ",
+    "jobkey": "066f952bb6793d2b",
+    "formattedRelativeTime": "4 days ago",
+  },
+  {
+    "jobtitle": "Senior Java Developer",
+    "company": "Bancvue",
+    "snippet": "The Senior <b>Java</b> Developer is responsible for the... field. 7-10 years of experience in <b>Java</b> EE development, including at least one <b>Java</b> framework (e.g. Spring... ",
+    "jobkey": "8276cdc5acba8b49",
+    "formattedRelativeTime": "3 days ago",
+  },
+  {
+    "jobtitle": "Web Development Software Engineer",
+    "company": "HomeAway",
+    "snippet": "application and supporting <b>Java</b> services with a focus... of high traffic <b>Java</b> web applications. * 1-3 years programming experience in <b>Java</b>. * 1-2 years experience... ",
+    "jobkey": "223bb8ebe75df8d0",
+    "formattedRelativeTime": "4 days ago",
+  },
+  {
+    "jobtitle": "Java Developer",
+    "company": "Neos Consulting Group, LLC",
+    "snippet": "Neos Consulting is hiring a <b>java</b> web services developer... should have at least 5 years experience in <b>java</b> development, and in building web services. Candidates... ",
+    "jobkey": "0cd0c0cc57b4b36b",
+    "formattedRelativeTime": "7 days ago",
+  },
+  {
+    "jobtitle": "Software Engineer",
+    "company": "AgileAssets",
+    "snippet": "technical skills • 3+ years experience building complex software systems • <b>Java</b> programming expertise • Prior experience in a Scrum or Kanban environment highly... ",
+    "jobkey": "08cff1b7bea99649",
+    "formattedRelativeTime": "6 days ago",
+  },
+  {
+    "jobtitle": "Software Engineer",
+    "company": "Yodle",
+    "snippet": "for the job. While <b>java</b> is our predominant language... take pride in what you build. Having programmed in <b>Java</b> will help you get going faster, but your primary... ",
+    "jobkey": "8560c47cdd63ea70",
+    "formattedRelativeTime": "14 hours ago",
+  },
+  {
+    "jobtitle": "MTS 1 Software Engineer PayPal",
+    "company": "eBay Inc.",
+    "snippet": "Edge technologies like <b>Java</b>, JavaScript, HTML5/CSS3... Asynchronous Events & Messaging Data Architecture <b>Java</b> Enterprise Work experience: Expert in designing... ",
+    "jobkey": "6df93622f7f5fb61",
+    "formattedRelativeTime": "10 days ago",
+  },
+  {
+    "jobtitle": "Software Engineer",
+    "company": "Cisco Systems",
+    "snippet": "Management principles. (DVCS) * A variety of languages including: assembly, C, <b>Java</b>, shell script, Perl, Ruby, Python, or equivalent Qualified candidates must... ",
+    "jobkey": "03f24a020b744ac6",
+    "formattedRelativeTime": "11 days ago",
+  }
+]
